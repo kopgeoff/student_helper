122f3fda5cae95ba70e199db74acae6c70bcca6a
mengfanhua <1070985939@qq.com>
2020-03-25 10:42:12 +0800

    课程表数据库部分

--- get_database/controlClassDatabase.py
+++ get_database/controlClassDatabase.py
@@ -5,15 +5,15 @@ try:
     conn = psycopg2.connect(database="classplandb", user="simple", password="123456", host="127.0.0.1", port="3389")
     cur = conn.cursor()
     try:
-        cur.execute("select * from users where id='%s'" % "20174529")
+        cur.execute("select * from users where id='%s'" % "123456")
         conn.commit()
         if len(cur.fetchall()) == 0:
-            cur.execute("insert into users(id,password) values('%s','%s')" % ("20174529", "woming123456"))
+            cur.execute("insert into users(id,password) values('%s','%s')" % ("123456", "123456123456"))
             conn.commit()
         else:
-            cur.execute("update users set password = '%s' where id = '%s'" % ("woming12345", "20174529"))
+            cur.execute("update users set password = '%s' where id = '%s'" % ("12345612345", "123456"))
             conn.commit()
-        cur.execute("select * from users where id='%s'" % "20174529")
+        cur.execute("select * from users where id='%s'" % "123456")
         conn.commit()
         s = cur.fetchall()
         print(s)
