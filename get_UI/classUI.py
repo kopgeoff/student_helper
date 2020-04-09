acdf1cb2ce18dbe45d8880a3ca93efd403c7f14c
mengfanhua <1070985939@qq.com>
2020-04-09 21:22:12 +0800

    课程界面背景

--- get_UI/classUI.py
+++ get_UI/classUI.py
@@ -3,6 +3,10 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
 
+urls = "./resources/1.jpg"
+qss = "#Class{border-image: url(%s);}" % urls
+
+
 class ClassWidget(QWidget):
     def __init__(self):
         super().__init__()
@@ -10,4 +14,12 @@ class ClassWidget(QWidget):
         f = QGridLayout()
         f.addWidget(button)
         self.setLayout(f)
+        self.setObjectName("Class")
+        self.setStyleSheet(qss)
 
+    def paintEvent(self, a0: QPaintEvent) -> None:
+        opt = QStyleOption()
+        opt.initFrom(self)
+        painter = QPainter(self)
+        # painter.setRenderHint(QtGui.QPainter.Antialiasing) # 反锯齿
+        self.style().drawPrimitive(QStyle.PE_Widget, opt, painter, self)
