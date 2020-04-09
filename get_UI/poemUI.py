365b672a252ee41b78f5e7365bb65b0148515dda
mengfanhua <1070985939@qq.com>
2020-04-09 21:24:03 +0800

    诗词界面背景

--- get_UI/poemUI.py
+++ get_UI/poemUI.py
@@ -3,6 +3,10 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
 
+urls = "./resources/1.jpg"
+qss = "#Poem{border-image: url(%s);}" % urls
+
+
 class PoemWidget(QWidget):
     def __init__(self):
         super().__init__()
@@ -10,4 +14,12 @@ class PoemWidget(QWidget):
         f = QGridLayout()
         f.addWidget(button)
         self.setLayout(f)
+        self.setObjectName("Poem")
+        self.setStyleSheet(qss)
 
+    def paintEvent(self, a0: QPaintEvent) -> None:
+        opt = QStyleOption()
+        opt.initFrom(self)
+        painter = QPainter(self)
+        # painter.setRenderHint(QtGui.QPainter.Antialiasing) # 反锯齿
+        self.style().drawPrimitive(QStyle.PE_Widget, opt, painter, self)
