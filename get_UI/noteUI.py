7fa701e1a2e9007cddf362095265814e08800615
mengfanhua <1070985939@qq.com>
2020-04-09 21:23:03 +0800

    备忘录设置背景

--- get_UI/noteUI.py
+++ get_UI/noteUI.py
@@ -3,6 +3,10 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
 
+urls = "./resources/1.jpg"
+qss = "#Note{border-image: url(%s);}" % urls
+
+
 class NoteWidget(QWidget):
     def __init__(self):
         super().__init__()
@@ -10,3 +14,12 @@ class NoteWidget(QWidget):
         f = QGridLayout()
         f.addWidget(button)
         self.setLayout(f)
+        self.setObjectName("Note")
+        self.setStyleSheet(qss)
+
+    def paintEvent(self, a0: QPaintEvent) -> None:
+        opt = QStyleOption()
+        opt.initFrom(self)
+        painter = QPainter(self)
+        # painter.setRenderHint(QtGui.QPainter.Antialiasing) # 反锯齿
+        self.style().drawPrimitive(QStyle.PE_Widget, opt, painter, self)
