83d35b50342a1236d52fc99659c9348c3f0f9972
mengfanhua <1070985939@qq.com>
2020-04-09 21:23:43 +0800

    设置界面背景

--- get_UI/settingUI.py
+++ get_UI/settingUI.py
@@ -3,6 +3,10 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
 
+urls = "./resources/1.jpg"
+qss = "#Setting{border-image: url(%s);}" % urls
+
+
 class SettingWidget(QWidget):
     def __init__(self):
         super().__init__()
@@ -10,4 +14,12 @@ class SettingWidget(QWidget):
         f = QGridLayout()
         f.addWidget(button)
         self.setLayout(f)
+        self.setObjectName("Setting")
+        self.setStyleSheet(qss)
 
+    def paintEvent(self, a0: QPaintEvent) -> None:
+        opt = QStyleOption()
+        opt.initFrom(self)
+        painter = QPainter(self)
+        # painter.setRenderHint(QtGui.QPainter.Antialiasing) # 反锯齿
+        self.style().drawPrimitive(QStyle.PE_Widget, opt, painter, self)
