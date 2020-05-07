9022c381fbc96cb904dcecb83f6ed302cc02fe36
mengfanhua <1070985939@qq.com>
2020-05-08 02:43:41 +0800

    实现显示背景图和每日一词的显示，需联网

--- get_UI/poemUI.py
+++ get_UI/poemUI.py
@@ -1,21 +1,32 @@
-from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
-
-
-urls = "./resources/1.jpg"
-qss = "#Poem{border-image: url(%s);}" % urls
+import requests
+from get_poem import getOnePoem
 
 
 class PoemWidget(QWidget):
     def __init__(self):
         super().__init__()
-        button = QPushButton("poem")
-        f = QGridLayout()
-        f.addWidget(button)
-        self.setLayout(f)
+        fontId = QFontDatabase.addApplicationFont("./fonts/汉仪糯米团简.ttf")
+        fontName = QFontDatabase.applicationFontFamilies(fontId)[0]
+        label = QLabel(getOnePoem.get_poem())
+        label.setStyleSheet("font-size:30px;")
+        label.setFont(QFont(fontName))
         self.setObjectName("Poem")
         self.setStyleSheet("#Poem{background-color:#bbddf0}")
+        url = getOnePoem.get_image()
+        if url != "":
+            req = requests.get(url)
+            image = req.content
+            req.close()
+            f = open("temp.jpg", "wb")
+            f.write(image)
+            f.close()
+            self.setStyleSheet("#Poem{background-color:#bbddf0; background-image:url('./temp.jpg');}")
+        f = QGridLayout()
+
+        f.addWidget(label)
+        self.setLayout(f)
 
     def paintEvent(self, a0: QPaintEvent) -> None:
         opt = QStyleOption()
